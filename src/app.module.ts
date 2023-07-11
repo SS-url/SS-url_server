e21cccc5945d4ce809b79ff3b21eb952d2c4e183
jisoook <jisoook@mymusictaste.com>
2023-07-11 16:57:01 +0900

    feat(mongoose): Add mongoose uri in an env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseService } from './DB/database.service';
 import { UrlController } from './Url/url.controller';
 import { UrlModule } from './Url/url.module';
 import { UrlService } from './Url/url.service';
 @Module({
-  imports: [UrlModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UrlModule,
+  ],
   controllers: [UrlController],
-  providers: [UrlService],
+  providers: [DatabaseService, UrlService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { DatabaseService } from './DB/database.service';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
+  const databaseService = app.get(DatabaseService);
+
+  // Connect to MongoDB
+  await databaseService.connect(process.env.MONGODB_URI, 'your-database-name');
+
+  // Start the Nest.js application
   await app.listen(3000);
 }
 bootstrap();
